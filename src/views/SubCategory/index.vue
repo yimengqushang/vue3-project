d340026cee704f56eae29f3c41003dd27faae155
Mr.chen <1294549818@qq.com>
2023-08-02 17:44:17 +0800

    add(SubCategory):Infinite list loading

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,16 +1,54 @@
 <script setup>
-import { getCategoryFilterAPI } from '@/api/category.js';
+import { getCategoryFilterAPI, getSubCategoryAPI } from '@/api/category.js';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import GoodsItem from '@/views/Home/components/Goodsitem.vue';
 
+// 获取面包屑导航数据
 const categoryData = ref({})
 const route = useRoute()
-
 const getCategoryData = async () => {
   const res = await getCategoryFilterAPI(route.params.id)
   categoryData.value = res.result
 }
 onMounted(() => getCategoryData())
+
+// 获取基础列表数据渲染
+const goodList = ref([])
+const reqData = ref({
+  categoryId: route.params.id,
+  page: 1,
+  pageSize: 20,
+  sortField: 'publishTime'
+})
+
+const getGoodList = async () => {
+  const res = await getSubCategoryAPI(reqData.value)
+  // console.log(res)
+  goodList.value = res.result.items
+}
+
+onMounted(() => getGoodList())
+
+// tab切换回调
+const tab = () => {
+  reqData.value.page = 1
+  getGoodList()
+}
+
+// 加载更多
+const disabled = ref(false)
+const load = async () => {
+  // 获取下一页数据
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value, ...res.result.items]
+  // 没有数据则停止加载
+  if (res.result.items.length === 0) {
+    disabled.value = true
+  }
+
+}
 
 </script>
 
@@ -26,13 +64,14 @@ onMounted(() => getCategoryData())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tab">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
         <!-- 商品列表-->
+        <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id"></GoodsItem>
       </div>
     </div>
   </div>
